94250b7f8442a1ba14e449b39afaede6b1d6da30
JustBanMe <brandon.p.gan@yzw.cn>
2021-01-13 11:12:27 +0800

    update

--- staging/src/App.js
+++ staging/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Hello from './components/Hello/Hello'
+// 组件文件以 index 命名可以不写文件名
 import Welcome from './components/Welcome'
 
 export default class App extends Component {
--- staging/src/components/Welcome/index.jsx
+++ staging/src/components/Welcome/index.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 
-import './index.css'
+// 为了避免样式冲突，将 css 文件加上 .module 表示模块化
+// 然后使用 import xxx from xxx.module.css 语法引入
+// 使用样式时使用 {xxx.样式名} 来调用
+import welcome from  './index.module.css'
 
 export default class Welcome extends Component {
     render() {
-        return <h2 className="header">Welcome</h2>
+        return <h2 className={welcome.title}>Welcome</h2>
     }
 }
